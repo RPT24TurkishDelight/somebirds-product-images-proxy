d63ab7472e8a846f70bd8afd0ff4a83dae2939e7
Ubuntu <ubuntu@ip-172-31-17-92.us-west-1.compute.internal>
2021-03-11 23:47:08 +0000

    server changes

--- proxy/server.js
+++ proxy/server.js
@@ -6,7 +6,7 @@ const port = 3000;
 const app = express();
 //change to local host if desired for environment 'http://localhost:3001' 'http://localhost:3002'
 const sizeColorServicePath = 'http://3.141.97.133:3001';
-const productServicePath = 'http://184.169.234.6:3002';
+const productServicePath = 'http://13.57.48.234:3002';
 const galleryServicePath = 'http://54.215.52.230:3004';
 // const galleryServicePath = 'http://localhost:3004';
 const feedbackServicePath = 'http://52.9.33.58:3003';
